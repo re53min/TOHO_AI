68fa070c6f641501ba53d07295027a89f83549ef
re53min <magicalpuyo2@gmail.com>
2016-11-15 14:16:52 +0900

    Update seq2seq.py

--- rnn/seq2seq.py
+++ rnn/seq2seq.py
@@ -11,14 +11,14 @@ class Seq2Seq(link.Chain):
     def __init__(self, n_input, n_feat, h_encode, h_decode, n_output, train=True):
         super(Seq2Seq, self).__init__(
             # encoder layer
-            input=L.EmbedID(n_input, n_feat, ignore_label=-1),
+            input_embed=L.EmbedID(n_input, n_feat, ignore_label=-1),
             encode1=L.StatefulGRU(n_feat, h_encode),
             encode2=L.StatefulGRU(h_encode, h_encode),
             # connection layer
             decode1=L.StatefulGRU(h_encode, h_decode),
             # decoder layer
             decode2=L.StatefulGRU(h_decode, h_decode),
-            
+            output_embed=L.EmbedID(n_output, n_feat),
             output=L.Linear(h_decode, n_output),
         )
         for param in self.params():
@@ -30,4 +30,17 @@ class Seq2Seq(link.Chain):
         self.encode2.reset_state()
         self.decode1.reset_state()
         self.decode2.reset_state()
+
+    def encode(self, input_sentence):
+
+        for word in input_sentence:
+            input_embed = F.tanh(self.input_embed(word))
+            enc1 = self.encode1(input_embed)
+            enc2 = self.encode2(enc1)
+            context = self.decode1(enc2)
+
+        return context
+
+    def decode(self, context, output_sentence, train):
+        
         
