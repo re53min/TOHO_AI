7ba3aed095996eccfd4c2520e5e7e5f8a26035d3
re53min <magicalpuyo2@gmail.com>
2016-12-01 00:12:11 +0900

    update seq2seq

--- rnn/seq2seq.py
+++ rnn/seq2seq.py
@@ -31,18 +31,17 @@ def make_vocab_dict(words):
 
 
 class Seq2Seq(link.Chain):
-    def __init__(self, n_input, n_feat, h_encode, h_decode, n_output, train=True):
+    def __init__(self, n_input, n_feat, n_hidden, n_output, train=True):
         super(Seq2Seq, self).__init__(
             # encoder layer
             input_embed=L.EmbedID(n_input, n_feat, ignore_label=-1),
-            encode1=L.StatefulGRU(n_feat, h_encode),
-            encode2=L.StatefulGRU(h_encode, h_encode),
-            # connection layer
-            decode1=L.StatefulGRU(h_encode, h_decode),
+            encode1=L.StatefulGRU(n_feat, n_hidden),
+            encode2=L.StatefulGRU(n_hidden, n_hidden),
             # decoder layer
-            decode2=L.StatefulGRU(h_decode, n_feat),
-            #output_embed=L.EmbedID(h_decode, n_feat),
-            output=L.Linear(n_feat, n_output),
+            output_embed=L.EmbedID(n_output, n_feat),
+            decode1=L.StatefulGRU(n_hidden, n_feat),
+            decode2=L.StatefulGRU(n_hidden, n_hidden),
+            output=L.Linear(n_hidden, n_output),
         )
         for param in self.params():
             param.data[...] = np.random.uniform(-0.1, 0.1, param.data.shape)
@@ -54,34 +53,44 @@ class Seq2Seq(link.Chain):
         self.decode1.reset_state()
         self.decode2.reset_state()
 
-    def encode(self, input_sentence):
+    def encode(self, sentences):
 
-        for word in input_sentence:
-            word = Variable(np.array([[word]], dtype=np.int32), volatile=False)
+        for word in sentences:
+            word = Variable(np.array([[word]], dtype=np.int32))
             input_embed = F.tanh(self.input_embed(word))
             enc1 = self.encode1(input_embed)
             enc2 = self.encode2(enc1)
 
         return enc2
 
-    def decode(self, context, teach_id, train):
+    def decode(self, sentences):
+        sentences = Variable(np.array([sentences], dtype=np.int32), volatile=False)
+        loss = Variable(self.xp.zeros((), dtype=self.xp.float32))
+        n_words = len(sentences)-1
 
-        decode0 = self.decode1(context)
-        decode1 = self.decode2(decode0)
-        # ouput_embded = F.tanh(self.output_embed(decode1))
-        output = self.output(decode1)
+        for word, t in zip(sentences, sentences[1:]):
+            decode0 = F.tanh(self.output_embed(word))
+            decode1 = self.decode1(decode0)
+            decode2 = self.decode2(decode1)
+            z = self.output(decode2)
 
-        if train:
-            t = Variable(np.array([teach_id], dtype=np.int32), volatile=False)
-            return F.softmax_cross_entropy(output, t), output
-        else:
-            return output
+            loss += F.softmax_cross_entropy(z, t)
 
-    # def __call__(self, x, t):
-    #    context = self.encode(x)
-    #    loss = self.decode(context=context, teach_id=t)
-    #
-    #    return loss
+        return loss, n_words
+
+    def init_decoder(self, contexts):
+        self.decode1.set_state(contexts)
+        self.decode2.set_state(contexts)
+
+    def __call__(self, x, t):
+        # encode
+        contexts = self.encode(x)
+
+        # decode
+        self.init_decoder(contexts)
+        loss, n_word = self.decode(t)
+
+        return loss
 
 if __name__ == "__main__":
 
@@ -94,7 +103,7 @@ if __name__ == "__main__":
     input_vocab = make_vocab_dict(input_sentence)  # inputs, input_vocab = make_vocab_dict(input_sentence)
     output_vocab = make_vocab_dict(output_sentence)  # outputs, output_vocab = make_vocab_dict(output_sentence)
 
-    model = Seq2Seq(n_input=len(input_vocab), n_feat=4, h_encode=10, h_decode=10, n_output=len(output_vocab))
+    model = Seq2Seq(n_input=len(input_vocab), n_feat=4, n_hidden=10, n_output=len(output_vocab))
     model.compute_accuracy = False
 
     # optimizerの設定
@@ -102,21 +111,16 @@ if __name__ == "__main__":
     optimizer.setup(model)
     optimizer.add_hook(chainer.optimizer.GradientClipping(5))  # 勾配の上限
 
+    loss = 0
+    model.reset_state()
+
     print('入力文: ' + ''.join(input_sentence[1:6]))
 
     for i in xrange(50):
 
-        model.reset_state()
         inputs = [input_vocab[word] for word in input_sentence]
-
-        context = model.encode(inputs)
-        loss = 0
-
-        for word in output_sentence:
-            output_id = output_vocab[word]
-            loss, output = model.decode(context, output_id, model.train)
-            context = output
-            loss += loss
+        outputs = [output_vocab[word] for word in output_sentence]
+        loss = model(inputs, outputs)
 
         print('train_loss = {}'.format(loss.data))
         model.cleargrads()
@@ -124,8 +128,5 @@ if __name__ == "__main__":
         loss.unchain_backward()
         optimizer.update()
 
-        if i % 2 == 0:
-            sentence = model.decode(context=context, teach_id=None, train=False)
-            # print('{}: {}'.format(i/5, output_vocab[np.argmax(sentence.data)]))
-            print(output_sentence[np.argmax(sentence.data)])
+
 
