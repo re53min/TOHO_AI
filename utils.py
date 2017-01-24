68ba3dcd49c72cce4d114fb53aee04f410f6b61e
re53min <magicalpuyo2@gmail.com>
2017-01-24 15:42:47 +0900

    t-sneの追加

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf_8 -*-
 
-from __future__ import print_function
 import codecs
 import numpy as np
+import pickle
+from time import time
 import matplotlib.pyplot as plt
-import cPickle as pickle
+from matplotlib import offsetbox
+from sklearn import (manifold, datasets, decomposition, ensemble,
+                     discriminant_analysis, random_projection)
 
 from mecab import mecab_wakati
 
@@ -58,6 +61,55 @@ def plot_loss(plot_loss):
     plt.savefig('plot_image\\loss.pdf')
 
     return
+
+
+def plot(type, layer):
+
+    if type == "t-sne":
+        plot_tsne()
+    elif type == "pca":
+        print("pca")
+
+
+def plot_tsne(vec):
+
+    # scikt-learnのt-sneによる次元削減と可視化
+    tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+    vec_tsne = tsne.fit_transform(vec)
+    t0 = time()
+
+    plot_embedding(vec_tsne, "t-SNE embedding of the digits (time %.2fs)" % (time() - t0))
+
+
+def plot_embedding(X, title=None):
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    X = (X - x_min) / (x_max - x_min)
+
+    plt.figure()
+    ax = plt.subplot(111)
+    for i in range(X.shape[0]):
+        plt.text(X[i, 0], X[i, 1], str(digits.target[i]),
+                 color=plt.cm.Set1(y[i] / 10.),
+                 fontdict={'weight': 'bold', 'size': 9})
+
+    if hasattr(offsetbox, 'AnnotationBbox'):
+        # only print thumbnails with matplotlib > 1.0
+        shown_images = np.array([[1., 1.]])  # just something big
+        for i in range(digits.data.shape[0]):
+            dist = np.sum((X[i] - shown_images) ** 2, 1)
+            if np.min(dist) < 4e-3:
+                # don't show points that are too close
+                continue
+            shown_images = np.r_[shown_images, [X[i]]]
+            imagebox = offsetbox.AnnotationBbox(
+                offsetbox.OffsetImage(digits.images[i], cmap=plt.cm.gray_r),
+                X[i])
+            ax.add_artist(imagebox)
+    plt.xticks([]), plt.yticks([])
+    if title is not None:
+        plt.title(title)
+
+    plt.savefig(title+".png")
 
 
 if __name__ == "__main__":
